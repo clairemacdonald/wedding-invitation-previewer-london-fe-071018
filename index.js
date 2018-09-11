de6e2d8cd75bd315159dd8adbf28f370d2fe0de0
clairemacdonald <claire.macdonald@blis.com>
2018-09-11 15:45:49 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ $('#input1,#input2, #input3, #input4, #input5').keyup(function(){
    console.log("fdsfds");
 });
 
+function changeimg() {
+  document.getElementById(sec1).style.backgroundImage="url(https://creative.blis.com/JamieInviteTest/pink.png)";
+}
+
