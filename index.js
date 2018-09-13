a709b2f0cee7e79cb895848a727f8b4f00096dc4
clairemacdonald <claire.macdonald@blis.com>
2018-09-13 19:04:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const fields = ["first-spouse", "second-spouse", "date", "location"];
 
 const images = {
   floral: "https://creative.blis.com/JamieInviteTest/pink.png",
-  vintage: "file:///Users/clairemacdonald/Desktop/Untitled-1.png",
+  vintage: "https:///Users/clairemacdonald/Desktop/Untitled-1.png",
   modern: "https://creative.blis.com/JamieInviteTest/circle.png",
 
 };
