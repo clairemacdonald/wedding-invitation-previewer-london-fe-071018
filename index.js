dba386c0a4a604c4fcb9c748ee89924ee4b86d9e
clairemacdonald <claire.macdonald@blis.com>
2018-09-13 15:32:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
 
 
-$('#input1,#input2, #input3, #input4, #input5').keyup(function(){
-   var $this = $(this);
-   $('.'+$this.attr('id')+'').html($this.val());
-   console.log("fdsfds");
-});
+//$('form input').keyup(function(){
+//   var $this = $(this);
+//   $('.'+$this.attr('id')+'').html($this.val());
+//   console.log("fdsfds");
+//});
 
-function changeimg() {
-  document.getElementById(sec1).style.backgroundImage="url(https://creative.blis.com/JamieInviteTest/pink.png)";
+//function changeimg() {
+  //document.getElementById(sec1).style.backgroundImage="url//(https://creative.blis.com/JamieInviteTest/pink.png)";
+//}
+
+
+
+const options = [
+  {theme: "floral", display: ""},
+  {theme: "vintage", display: ""},
+  {theme: "modern", display: ""},
+
+];
+
+const fields = ["first-spouse", "second-spouse", "date", "location"];
+
+const images = {
+  floral: "https://creative.blis.com/JamieInviteTest/pink.png",
+  vintage: "https://creative.blis.com/JamieInviteTest/pattern.png",
+  modern: "https://creative.blis.com/JamieInviteTest/circle.png",
+
 }
 
+document.addEventListener("DOMContentLoaded", ()=>{
+  const optionsContainer = document.getElementsByClassName("options")[0];
+  
+  const displayTitle = document.getElementById("display-title");
+  const previewImage = document.getElementById("preview-img");
+  
+  const renderCard = theme => {
+    displayTitle.innerHTML = `Current Theme: ${theme}`;
+    console.log(!previewImage.src);
+    if (!previewImage.src) document.getElementsByTagName("form")[0].classList.add("active");
+    console.log(document.getElementsByTagName("form")[0].classList);
+    previewImage.src = images[theme];
+    previewImage.alt = theme;
+  }
+
+  options.forEach(option => {
+    const button = document.createElement("button");
+    button.id = option.theme;
+    button.innerHTML = option.theme;
+    button.addEventListener("click", () => renderCard(option.theme));
+    optionsContainer.appendChild(button);
+  })
+  
+  fields.forEach(field => {
+    document.getElementById(field).addEventListener("input", e => {
+      console.log(e, e.target, e.target.value)
+      document.getElementById(`display-${field}`).innerHTML = e.target.value;
+    })
+  })
+});
+
+
+
+ 
